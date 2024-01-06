c520ac4e00d81167f959ca70be710c071f40e518
Andrei <vertru112211@gmail.com>
2024-01-06 11:31:45 +0200

    added

--- HNY/main/urls.py
+++ HNY/main/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('card/<str:name>', views.card)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- HNY/main/views.py
+++ HNY/main/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from random import choice
+
+
+new_year_greetings = [
+    "З Новим Роком! Нехай цей рік принесе вам щастя та радість!",
+    "Щасливого Нового Року! Бажаємо вам яскравих святкових моментів!",
+    "Новорічні вітання! Нехай вам супроводжує удача в усьому!",
+    "Зичимо радості, здоров'я та успіхів у новому році!",
+    "Веселих свят і щасливого Нового Року! Нехай всі ваші мрії здійсняться!",
+    "Зичимо вам світлих свят та яскравих вражень у новому році!",
+    "Щасливого Нового Року! Нехай кожен день буде наповнений радістю!",
+    "Бажаємо вам купу радості та море веселих моментів у новому році!",
+    "З новим щастям! Нехай кожен день приносить вам тільки позитивні емоції!",
+    "Новорічні привітання! Нехай у вашому житті буде багато світлих подій та удачі!"
+]
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    if request.method == 'GET':
+        return render(request, 'main/index.html')
+    else:
+        name = request.POST.get('name_inp')
+        return redirect(f'/card/{name}')
+
+
+def card(request, name):
+    return render(request, 'main/card.html', {'message': choice(new_year_greetings), 'name': name})
+
